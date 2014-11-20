f18572cb870ed0e74f270466536a8ae7c1cbfe0c
gabriele.pessoa <gabriele.pessoa@gmail.com>
2014-11-20 18:07:39 +0000

    atualização de imagens

--- Codigo/Abrace Um PET - V2/src/usuario/gui/TelaInicialGUI.java
+++ Codigo/Abrace Um PET - V2/src/usuario/gui/TelaInicialGUI.java
@@ -3,13 +3,13 @@ package usuario.gui;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingConstants;
 import javax.swing.border.EmptyBorder;
 
+import usuario.gui.imagem.Imagens;
 import adotante.gui.AdotanteGUI;
 
 public class TelaInicialGUI extends JFrame {
@@ -75,7 +75,7 @@ public class TelaInicialGUI extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		button.setIcon(new ImageIcon("https://abrace-um-pet.googlecode.com/svn/trunk/Codigo/Abrace%20Um%20PET%20-%20V2/imagens/cadastro.png"));
+		Imagens.imagemCadastro(button);
 		button.setBounds(37, 73, 555, 275);
 		telaInicialGUI.add(button);
 	}
