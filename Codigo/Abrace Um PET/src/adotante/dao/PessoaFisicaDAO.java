8fb542e194b07796af55511a09bc0fbd9b3b4ae0
pr.francaf <pr.francaf@gmail.com>
2014-11-23 23:45:40 +0000

    PessoaFisicaDAO

--- Codigo/Abrace Um PET/src/adotante/dao/PessoaFisicaDAO.java
+++ Codigo/Abrace Um PET/src/adotante/dao/PessoaFisicaDAO.java
@@ -21,9 +21,10 @@ public class PessoaFisicaDAO {
 	 */
 	public boolean adicionarPessoFisica(PessoaFisica pessoaFisica) {
 		try {
+			int id;
 			Connection con = Conexao.abrirConceccaoMySQL();
 
-			String query = "INSERT INTO endereco (estado, cidade, bairro, rua, numero, cep, complemento) values (?, ?, ?, ?, ?, ?, ?)";
+			String query = "insert into endereco (estado, cidade, bairro, rua, numero, cep, complemento) values (?, ?, ?, ?, ?, ?, ?)";
 
 			PreparedStatement preparedStatement = (PreparedStatement) con
 					.prepareStatement(query);
@@ -34,17 +35,82 @@ public class PessoaFisicaDAO {
 					.getEndereco().getCidade());
 			preparedStatement.setString(3, pessoaFisica.getAdotante()
 					.getEndereco().getBairro());
-			preparedStatement.setString(3, pessoaFisica.getAdotante()
+			preparedStatement.setString(4, pessoaFisica.getAdotante()
 					.getEndereco().getRua());
-			preparedStatement.setString(3, pessoaFisica.getAdotante()
+			preparedStatement.setString(5, pessoaFisica.getAdotante()
 					.getEndereco().getNumero());
-			preparedStatement.setString(3, pessoaFisica.getAdotante()
+			preparedStatement.setString(6, pessoaFisica.getAdotante()
 					.getEndereco().getCep());
-			preparedStatement.setString(3, pessoaFisica.getAdotante()
+			preparedStatement.setString(7, pessoaFisica.getAdotante()
 					.getEndereco().getComplemento());
-			preparedStatement.execute();  
-			preparedStatement.close(); 
 
+			int affectedRows = preparedStatement.executeUpdate();
+
+	        if (affectedRows == 0) {
+	            throw new SQLException("Creating user failed, no rows affected.");
+	        }
+
+	        try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+	            if (generatedKeys.next()) {
+	            	id = (int) generatedKeys.getLong(1);
+	            }
+	            else {
+	                throw new SQLException("Creating user failed, no ID obtained.");
+	            }
+	        }
+			preparedStatement.close(); 
+			
+			
+			
+			query = "insert into adotante (nome, idEndereco, telefoneFixo, telefoneCelular, email) values (?, ?, ?, ?, ?)";
+			preparedStatement = (PreparedStatement) con.prepareStatement(query);
+			
+			preparedStatement.setString(1, pessoaFisica.getAdotante().getNome());
+			preparedStatement.setInt(2, id);
+			preparedStatement.setString(3, pessoaFisica.getAdotante().getTelefoneFixo());
+			preparedStatement.setString(4, pessoaFisica.getAdotante().getTelefoneCelular());
+			preparedStatement.setString(5, pessoaFisica.getAdotante().getEmail());
+			
+			affectedRows = preparedStatement.executeUpdate();
+
+	        if (affectedRows == 0) {
+	            throw new SQLException("Creating user failed, no rows affected.");
+	        }
+
+	        try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+	            if (generatedKeys.next()) {
+	            	id = (int) generatedKeys.getLong(1);
+	            }
+	            else {
+	                throw new SQLException("Creating user failed, no ID obtained.");
+	            }
+	        }
+			preparedStatement.close(); 
+			
+			query = "insert into pessoafisica (rg, cpf, idAdotante) values (?, ?, ?)";
+			preparedStatement = (PreparedStatement) con.prepareStatement(query);
+			
+			preparedStatement.setString(1, pessoaFisica.getRg());
+			preparedStatement.setString(2, pessoaFisica.getCpf());
+			preparedStatement.setInt(3, id);
+			
+			affectedRows = preparedStatement.executeUpdate();
+
+	        if (affectedRows == 0) {
+	            throw new SQLException("Creating user failed, no rows affected.");
+	        }
+
+	        try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+	            if (generatedKeys.next()) {
+	            	id = (int) generatedKeys.getLong(1);
+	            }
+	            else {
+	                throw new SQLException("Creating user failed, no ID obtained.");
+	            }
+	        }
+			preparedStatement.close(); 
+			
+			
 			Conexao.fecharConecaoMySQL();
 			return true;
 		} catch (Exception ex) {
