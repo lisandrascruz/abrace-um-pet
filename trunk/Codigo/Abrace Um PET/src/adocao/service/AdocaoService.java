d6d2f0156801f31c378a8ff97096e86c3901e9a3
pr.francaf@gmail.com <pr.francaf@gmail.com@8ee5bc82-10a1-c35d-0bb9-652f10d06cf8>
2014-12-17 15:47:42 +0000

    devolução de animal

--- trunk/Codigo/Abrace Um PET/src/adocao/service/AdocaoService.java
+++ trunk/Codigo/Abrace Um PET/src/adocao/service/AdocaoService.java
@@ -14,13 +14,40 @@ public class AdocaoService {
 	}
 	
 	public Adotante consultarAdotante(Pessoa pessoa){
-		Adotante adotante = new Adotante();
-		AdotanteDAO AdotanteDAO = new AdotanteDAO();
+		Adotante adotante = null;
+		AdotanteDAO adotanteDAO = new AdotanteDAO();
 		try{
-			adotante = AdotanteDAO.consultarAdotante(pessoa);
+			adotante = adotanteDAO.consultarAdotante(pessoa);
 		} catch (Exception ex) {
 			
 		}
 		return adotante;
+	}
+	
+	public Adocao consultarAdocao(String cpf, String rga){
+		Adocao adocao = null;
+		AdocaoDAO adocaoDAO = new AdocaoDAO();
+		try{
+			adocao = adocaoDAO.consultarAdocao(cpf, rga);
+		} catch (Exception ex) {
+			
+		}
+		return adocao;
+	}
+	
+	public Adocao consultarAdocaoJuridica(String cnpj, String rga){
+		Adocao adocao = null;
+		AdocaoDAO adocaoDAO = new AdocaoDAO();
+		try{
+			adocao = adocaoDAO.consultarAdocaoJuridica(cnpj, rga);
+		} catch (Exception ex) {
+			
+		}
+		return adocao;
+	}
+	
+	public boolean editarAdocao(Adocao adocao){
+		AdocaoDAO adocaoDAO = new AdocaoDAO();
+		return adocaoDAO.editarAdocao(adocao);
 	}
 }
